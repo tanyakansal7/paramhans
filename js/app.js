e080fc640ecce53ccc378310c0fc96da70abca6d
tanyakansal7 <you@example.com>
2021-08-29 16:53:26 +0530

    made this basic website for practice puropse

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// **************************CAROUSEL********************************
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// ******************************LOGIN FORM***************************
+let profile=document.getElementById("profile");
+profile=profile.addEventListener("click",(e)=>{
+  document.getElementById("myForm").style.display="block";
+})
+let cancel=document.getElementById("cancel");
+cancel=cancel.addEventListener("click",(e)=>{
+  document.getElementById("myForm").style.display="none";
+})
